fafbbbcdf925187c16e216c3d4aca0edf0a6169b
Alex <84599917+GyverLibs@users.noreply.github.com>
2022-01-07 20:28:01 +0300

    upd

--- src/microOneWire.cpp
+++ src/microOneWire.cpp
@@ -1,31 +1,29 @@
 #include "microOneWire.h"
+#ifdef __AVR__
+#define MOW_CLI() uint8_t oldSreg = SREG; cli();
+#define MOW_SEI() SREG = oldSreg
+#else
+#define MOW_CLI()
+#define MOW_SEI()
+#endif
 
 /* ~10722CK ~670.1250 us (16MHZ) */
 /* max time without interrupts ~90us*/
 bool oneWire_reset(uint8_t pin) { 
     pinMode(pin, 1);
     delayMicroseconds(520);
-    #ifdef AVR
-    uint8_t oldSreg = SREG;
-    cli();
-    #endif
+    MOW_CLI();
     pinMode(pin, 0);
     delayMicroseconds(2);
     for (uint8_t c = 80; c; c--) {
         if (!digitalRead(pin)) {
-            #ifdef AVR
-            SREG = oldSreg;
-            #endif
-            for (uint8_t i = 200; !digitalRead(pin) && i; i--) {
-                delayMicroseconds(1);
-            }
+            MOW_SEI();
+            for (uint8_t i = 200; !digitalRead(pin) && i; i--) delayMicroseconds(1);            
             return false;
         }
         delayMicroseconds(1);
     }
-    #ifdef AVR
-    SREG = oldSreg;
-    #endif
+    MOW_SEI();
     return true;
 }
 
@@ -33,12 +31,9 @@ bool oneWire_reset(uint8_t pin) {
 /* ~9150CK ~571.8750 us (16MHZ) */
 /* max time without interrupts ~70us*/
 void oneWire_write(uint8_t data, uint8_t pin) {
-    for (uint8_t p = 8; p; p--) {
-        #ifdef AVR
-        uint8_t oldSreg = SREG;
-        cli();
-        #endif
+    for (uint8_t p = 8; p; p--) {        
         pinMode(pin, 1);
+		MOW_CLI();
         if (data & 1) {
             delayMicroseconds(5);
             pinMode(pin, 0);
@@ -48,9 +43,7 @@ void oneWire_write(uint8_t data, uint8_t pin) {
             pinMode(pin, 0);
             delayMicroseconds(5);
         }
-        #ifdef AVR
-        SREG = oldSreg;
-        #endif
+        MOW_SEI();
         data >>= 1;
     }
 }
@@ -61,19 +54,14 @@ uint8_t oneWire_read(uint8_t pin) {
     uint8_t data = 0;
     for (uint8_t p = 8; p; p--) {
         data >>= 1;
-        #ifdef AVR
-        uint8_t oldSreg = SREG;
-        cli();
-        #endif
+        MOW_CLI();
         pinMode(pin, 1);
         delayMicroseconds(2);
         pinMode(pin, 0);
         delayMicroseconds(8);
         if (digitalRead(pin)) data |= 1 << 7;
         delayMicroseconds(60);
-        #ifdef AVR
-        SREG = oldSreg;
-        #endif
+        MOW_SEI();
     }
     return data;
 }
--- src/microOneWire.h
+++ src/microOneWire.h
@@ -1,5 +1,6 @@
-// v1.2 от 26.04.2020. Повышена стабильность
-// v1.3 от 08.09.2021. Разбил на .h и .cpp
+// v1.2 от 26.04.2020 - Повышена стабильность
+// v1.3 от 08.09.2021 - Разбил на .h и .cpp
+// v1.4 от 07.01.2022 - Чуть сократил код
 
 #ifndef _microOneWire_h
 #define _microOneWire_h
@@ -8,5 +9,4 @@
 bool oneWire_reset(uint8_t pin);
 void oneWire_write(uint8_t data, uint8_t pin);
 uint8_t oneWire_read(uint8_t pin);
-
 #endif
